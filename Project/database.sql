13ccf5fa1de12a39f0e575a02ad7dec78cca08c5
kevin <kevin@kevin_pc>
2017-12-05 08:49:44 +0100

    commit na realm inorde

--- Project/database.sql
+++ Project/database.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS gebruikers;
 
 
 CREATE TABLE gebruikers (
-	gebruiker_id int,						
+	gebruiker_id varchar(20),
+        paswoord varchar(20),						
 	voornaam varchar(20),
 	achternaam varchar(20),
 	PRIMARY KEY(gebruiker_id)
@@ -12,21 +13,24 @@ CREATE TABLE gebruikers (
 
 CREATE TABLE groepsindeling (
 	indeling_id int AUTO_INCREMENT,
-	gebruiker_id int,
+	gebruiker_id varchar(20),
 	PRIMARY KEY(indeling_id),
 	FOREIGN KEY(gebruiker_id) REFERENCES gebruikers(gebruiker_id)
 	
 );
 
 CREATE TABLE groepen (
-	id int,
+	id varchar(20),
 	soort	varchar(20),
 	PRIMARY KEY(id),
 	FOREIGN KEY(id) REFERENCES gebruikers(gebruiker_id)
 );
 
 
+INSERT INTO gebruikers VALUES ('111','111','Nick','Todts');
+INSERT INTO gebruikers VALUES ('222','222','Kevin','Todts');
+INSERT INTO gebruikers VALUES ('999','999','Herman','Crauwels');
 
-
-
-
+INSERT INTO groepen VALUES ('111','student');
+INSERT INTO groepen VALUES ('222','student');
+INSERT INTO groepen VALUES ('999','docent');
